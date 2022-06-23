0270d1ef29b7a4f12308183d023e1a6c71696b3d
Paige Niedringhaus <paigen11@gmail.com>
2022-06-23 15:59:55 -0400

    updating sensor code

--- HuzzahSketch.ino
+++ HuzzahSketch.ino
@@ -1,4 +1,4 @@
-//sample code for Huzzah32 with Notecarrier A attached via breadboard and jumper wires
+// sample code for Huzzah32 with Notecarrier A attached via breadboard and jumper wires
 #include <Notecard.h>
 
 #include <NotecardPseudoSensor.h>
@@ -7,12 +7,13 @@ using namespace blues;
 #define usbSerial Serial
 #define txRxPinsSerial Serial1
 
-Notecard notecard; // not used but required by NotecardPseudoSensor library         
+Notecard notecard; // not used but required by NotecardPseudoSensor library
 NotecardPseudoSensor sensor(notecard);
 
 #define NOTE_PRODUCT_UID "com.blues.pniedringhaus:huzzah32_notecarrier_a"
 
-void setup() {
+void setup()
+{
   usbSerial.begin(115200);
   usbSerial.println("Starting...");
 
@@ -26,11 +27,12 @@ void setup() {
 
   // serial implementation (no note-arduino library required)
   // txRxPinsSerial.begin(9600);
+  // notecard.begin();
   // txRxPinsSerial.println("\n");
 
   // txRxPinsSerial.println("{\"req\":\"hub.set\",\"product\":\"" NOTE_PRODUCT_UID "\",\"mode\":\"continuous\"}");
 
-  // delay(50);
+  // delay(250);
   // while(txRxPinsSerial.available() > 0) {
   //   char incomingByte = txRxPinsSerial.read();
   //   if(incomingByte != '\r' && incomingByte != '\n') {
@@ -40,7 +42,8 @@ void setup() {
   // usbSerial.println();
 }
 
-void loop() {
+void loop()
+{
   float temperature = sensor.temp();
   float humidity = sensor.humidity();
 
@@ -55,40 +58,42 @@ void loop() {
 
   // note-arduino library implementation
   J *req = notecard.newRequest("note.add");
-  if (req != NULL) {
+  if (req != NULL)
+  {
     JAddStringToObject(req, "file", "sensors.qo");
     JAddBoolToObject(req, "sync", true);
     J *body = JAddObjectToObject(req, "body");
-    if (body) {
+    if (body)
+    {
       JAddNumberToObject(body, "temp", temperature);
       JAddNumberToObject(body, "humidity", humidity);
     }
     notecard.sendRequest(req);
   }
 
-// serial implementation (no note-arduino library required)
-// char json_request[150];
-// snprintf(json_request, sizeof(json_request), 
-//   "{"
-//     "\"req\":\"note.add\","
-//     "\"file\":\"sensors.qo\","
-//     "\"sync\":true,"
-//     "\"body\":{"
-//       "\"temp\":%d.%02d,"
-//       "\"humidity\":%d.%02d"
-//     "}"
-//   "}",
-//     (int)temperature, abs(((int)(temperature*100.0)%100)),
-//     (int)humidity, (int)(humidity*100.0)%100
-// );
+  // serial implementation (no note-arduino library required)
+  // char json_request[150];
+  // snprintf(json_request, sizeof(json_request),
+  //   "{"
+  //     "\"req\":\"note.add\","
+  //     "\"file\":\"sensors.qo\","
+  //     "\"sync\":true,"
+  //     "\"body\":{"
+  //       "\"temp\":%d.%02d,"
+  //       "\"humidity\":%d.%02d"
+  //     "}"
+  //   "}",
+  //     (int)temperature, abs(((int)(temperature*100.0)%100)),
+  //     (int)humidity, (int)(humidity*100.0)%100
+  // );
 
-// txRxPinsSerial.println(json_request);
-// delay(50); // wait for the Notecard to respond
-// while (txRxPinsSerial.available() > 0) {
-//     char incomingByte = txRxPinsSerial.read();
-//     if (incomingByte != '\r' && incomingByte != '\n') {
-//         usbSerial.print(incomingByte);
-//     }
-// }
-// usbSerial.println();
+  // txRxPinsSerial.println(json_request);
+  // delay(250); // wait for the Notecard to respond
+  // while (txRxPinsSerial.available() > 0) {
+  //     char incomingByte = txRxPinsSerial.read();
+  //     if (incomingByte != '\r' && incomingByte != '\n') {
+  //         usbSerial.print(incomingByte);
+  //     }
+  // }
+  // usbSerial.println();
 }
